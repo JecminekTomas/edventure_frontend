60d2f3962cbf12741004e215a1ab082848997d8a
jecminek_tomas <jecmto@gmail.com>
2021-11-02 12:51:10 +0100

    feat: Login and TopBar

---
+++ src/components/TopBar.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container fluid>
+    <v-app-bar app>
+      <v-app-bar-nav-icon class="hidden-lg-and-up" @click.stop="drawer = !drawer"/>
+
+      <v-toolbar-title class="mr-10 ml-2 font-weight-medium text-h5">EDVENTURE</v-toolbar-title>
+
+      <v-toolbar-items class="hidden-md-and-down" v-for="item in menuItems" :key="item.title" :to="item.path">
+        <v-btn text>
+        <v-icon class="pr-2">
+          {{ item.icon }}
+        </v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+
+      <v-spacer/>
+
+      <v-toolbar-items>
+        <v-btn fab x-large elevation="0" class="hidden-md-and-down">
+          <v-icon>mdi-account-circle-outline</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-title class="font-weight-medium mx-5 hidden-md-and-down">
+        Jméno Uživatele
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
+    >
+      <v-list nav dense>
+        <v-list-item-group v-model="group">
+          <v-list-item class="text-center">
+            <v-list-item-title>Jméno Uživatel</v-list-item-title>
+          </v-list-item>
+
+          <v-divider/>
+
+          <v-list-item class="py-4">
+            <v-list-item-icon>
+              <v-icon>mdi-account-search</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Hledat doučování</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item class="pb-4">
+            <v-list-item-icon>
+              <v-icon>mdi-note-plus-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Nová nabídka</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    menuItems: [
+      {title: 'Hledat doučování', path: '/home', icon: 'mdi-account-search'},
+      {title: 'Nová nabídka', path: '/signup', icon: 'mdi-note-plus-outline'},
+    ]
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col md="12" lg="4">
+        <v-card elevation="10" outlined>
+          <v-card-title>Vstupit do aplikace</v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Login" prepend-icon="mdi-account" color="secondary"/>
+              <v-text-field label="Heslo" prepend-icon="mdi-key" color="secondary"/>
+            </v-form>
+          </v-card-text>
+          <v-card-text class="my-n4">
+            <v-btn x-small text plain>
+              Nejste ještě registrován?
+            </v-btn>
+          </v-card-text>
+          <v-card-actions class="px-5">
+            <v-btn text class="orange--text">
+              Přihlásit se
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+
+</style>
